4a3935cbc3b633bd8af81b49ab99936ba6f768d6
Randy Odoom <randyodoom19@gmail.com>
2022-11-13 12:28:15 +0000

    register

--- screens/Hostel/Register.js
+++ screens/Hostel/Register.js
@@ -2,29 +2,99 @@ import { ScrollView, StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
 import { Colors, StyledButton, StyledButtonText } from "../../utils/styles";
 import CustomTextInput from "../../components/CustomTextInput";
+import { addDoc, collection, setDoc } from "firebase/firestore";
+import { auth } from "../../firebase";
 
-const Register = () => {
+const Register = ({route}) => {
+  const [hostelName] = useState(route?.params?.name);
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [studentId, setIdStudent] = useState("");
+  const [studentId, setStudentId] = useState("");
   const [contact, setContact] = useState("");
-  const [program, setLrogram] = useState("");
+  const [program, setProgram] = useState("");
   const [level, setLevel] = useState("");
-  const [guradingName, setGuardingName] = useState("");
-  const [guardianContact, setguardianContact] = useState("");
+  const [guardianName, setGuardianName] = useState("");
+  const [guardianContact, setGuardianContact] = useState("");
 
+  const valid =
+    firstName != "" &&
+    lastName != "" &&
+    studentId != "" &&
+    contact != "" &&
+    program &&
+    level != "" &&
+    guardianName != "" &&
+    guardianContact != "";
+
+  const submit = async () => {
+    setDoc(
+      doc(db, "UserInfo", "hostels", hostelName, auth?.currentUser?.email),
+      {
+        hostelName,
+        firstName,
+        lastName,
+        studentId,
+        contact,
+        program,
+        level,
+        guardianName,
+        guardianContact,
+      }
+    ).catch((error) => alert(error.message));
+    setLoading(<StyledButtonText>Submit</StyledButtonText>);
+  };
   return (
     <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
       <Text style={styles.space}></Text>
-      <CustomTextInput label="First name" icon="user"/>
-      <CustomTextInput label="Last name" icon="user"/>
-      <CustomTextInput label="Student ID" icon="credit-card"/>
-      <CustomTextInput label="Contact" icon="phone-call"/>
-      <CustomTextInput label="Program" icon="layout"/>
-      <CustomTextInput label="Level"icon="layers" />
-      <CustomTextInput label="Guardian Name" icon="user"/>
-      <CustomTextInput label="Guardian Contact" icon="phone-call"/>
-      <StyledButton style={styles.button}>
+      <CustomTextInput
+        label="First name"
+        icon="user"
+        onChangeText={(text) => setFirstName(text)}
+        value={firstName}
+      />
+      <CustomTextInput
+        label="Last name"
+        icon="user"
+        onChangeText={(text) => setLastName(text)}
+        value={lastName}
+      />
+      <CustomTextInput
+        label="Student ID"
+        icon="credit-card"
+        onChangeText={(text) => setStudentId(text)}
+        value={studentId}
+      />
+      <CustomTextInput
+        label="Contact"
+        icon="phone-call"
+        onChangeText={(text) => setContact(text)}
+        value={contact}
+      />
+      <CustomTextInput
+        label="Program"
+        icon="layout"
+        onChangeText={(text) => setProgram(text)}
+        value={program}
+      />
+      <CustomTextInput
+        label="Level"
+        icon="layers"
+        onChangeText={(text) => setLevel(text)}
+        value={level}
+      />
+      <CustomTextInput
+        label="Guardian Name"
+        icon="user"
+        onChangeText={(text) => setGuardianName(text)}
+        value={guardianName}
+      />
+      <CustomTextInput
+        label="Guardian Contact"
+        icon="phone-call"
+        onChangeText={(text) => setGuardianContact(text)}
+        value={guardianContact}
+      />
+      <StyledButton style={styles.button} disabled={!valid}>
         <StyledButtonText>Register</StyledButtonText>
       </StyledButton>
     </ScrollView>
